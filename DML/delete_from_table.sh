08c05331222feaacb4b748561141ddae44f79382
TareKelKhateb <tarekelkhateb31@gmail.com>
2025-11-29 18:50:16 +0200

    deleta and select script updated

--- DML/delete_from_table.sh
+++ DML/delete_from_table.sh
@@ -1,4 +1,172 @@
 #! /usr/bin/bash
 
+#
+source ./config.sh
+connectedDB=$1
 
-echo "delete from table script! ${1} database"
+
+exit_flag=0
+delete_table(){
+
+	#cat /tmp/$1
+	echo "*******************"	
+	while true 
+	do 
+		read -p "Enter Table Name" TableName
+		
+		if [ -f $default_path/$connectedDB/tables/$TableName ]
+		then
+			break 
+		else
+			echo "table doesn't exist "
+		fi
+	done 	
+	
+	cp $default_path/$connectedDB/tables/$TableName  /tmp/$TableName
+	
+	while true
+			do
+				
+			columns=$(cut -d ":" -f 1,2 "$default_path/$connectedDB/metadata/${TableName}_meta")
+			num_of_columns=$(wc -l < "$default_path/$connectedDB/metadata/${TableName}_meta")
+			echo "$columns" | nl -s " - " -w 1
+			read -p "enter the column number u want to be conditioned at or (n) to display result : " conditioned_column
+			
+			
+			if [ $conditioned_column -gt  $num_of_columns  ] 
+			then
+				echo "invalid number"
+				continue  
+			fi  
+			
+			
+			
+			if [ $conditioned_column = "n" ] 
+			then
+				break 
+			fi  
+			C_type=$(sed -n "${conditioned_column}p" <<< "$columns" | awk -F: '{print $2}')
+			echo $C_type
+			if [ $C_type = INT ]
+			then
+				select option in "==" ">" "<"; do
+				    case $option in
+					"==")
+					    
+					    read -p "enter value : " value
+					    # awk comparsion
+					    awk -F: -v value="$value" -v column="$conditioned_column" '{ if ($column != value) print }' /tmp/$TableName  >  /tmp/con_temp
+					    mv /tmp/con_temp /tmp/$TableName
+					    
+					    read -p "do u want to add another condition (y|n)"  conTinue
+					    
+							      
+					    if [ $conTinue = "y" ] 
+					    then
+					    		break 
+					    
+					    else 
+					    		break 2 
+					    
+					    fi  
+					    
+					    
+					    
+					    ;;
+					">")
+					    
+					
+					    read -p "enter value : " value
+					    # awk comparsion
+					    awk -F: -v value="$value" -v column="$conditioned_column" '{ if ($column <= value) print }' /tmp/$TableName  >  /tmp/con_temp
+					    mv /tmp/con_temp /tmp/$TableName
+					    
+					    read -p "do u want to add another condition (y|n)"  conTinue
+					    
+							      
+					    if [ $conTinue = "y" ] 
+					    then
+					    		break 
+					    
+					    else 
+					    		break 2 
+					    
+					    fi  
+					    
+					    
+					
+					
+					    break
+					    ;;
+					"<")
+					
+					    read -p "enter value : " value
+					    # awk comparsion
+					    awk -F: -v value="$value" -v column="$conditioned_column" '{ if ($column >= value) print }' /tmp/$TableName  >  /tmp/con_temp
+					    mv /tmp/con_temp /tmp/$TableName
+					    
+					    read -p "do u want to add another condition (y|n)"  conTinue
+					    
+							      
+					    if [ $conTinue = "y" ] 
+					    then
+					    		break 
+					    
+					    else 
+					    		break 2 
+					    
+					    fi  
+					    
+					    
+					    
+					    break
+					    ;;
+					*)
+					    echo "Invalid option. Try again."
+					    ;;
+				    esac
+				done
+			   else 
+			   # is string 
+			   select option in "==" ; do
+				    case $option in
+					"==")
+					    read -p "enter value : " value
+					    # awk comparsion
+					    awk -F: -v value="$value" -v column="$conditioned_column" '{ if ($column != value) print }' /tmp/$TableName  >  /tmp/con_temp
+					    mv /tmp/con_temp /tmp/$TableName
+					    
+					    read -p "do u want to add another condition (y|n)"  conTinue
+					    
+							      
+					    if [ $conTinue = "y" ] 
+					    then
+					    		break 
+					    
+					    else 
+					    		break 2 
+					    
+					    fi  
+					    
+					    
+					    
+					    ;;
+					"*")
+						"invalid"
+						;;
+			   	      esac
+			   done
+			   
+			   
+			fi
+				
+		 done
+		 echo  /tmp/$TableName
+		 echo  $default_path/$connectedDB/tables/$TableName
+		 mv /tmp/$TableName $default_path/$connectedDB/tables/$TableName
+		 
+		 
+}
+		
+delete_table  
+
--- DML/select_from_table.sh
+++ DML/select_from_table.sh
@@ -1,3 +1,265 @@
 #! /usr/bin/bash
+#
+source ./config.sh
+connectedDB=$1
 
-echo "select from table script!, ${1} database"
+exit_flag=0
+
+select_display_menu(){
+		 # $1 ->table
+		 # $2 -> table_data
+		 # $3 -> meta_data
+
+
+	select option in "diplay_all" "specific_columns" ; do
+	    case $option in
+		"diplay_all")
+			cat  $2
+			return
+		    ;;
+		"specific_columns")
+			# get the columns 
+			columns=$(cut -d ":" -f 1 "$3")
+			echo "$columns" | nl -s " - " -w 1
+			num_of_columns=$(wc -l < "$3")
+			
+			
+			declare -a column_to_display
+			while true
+			do 	
+			
+				read -p "enter the column number u want to display or type (n) to finish" column_num
+				# ToDo: check the input 
+				if [ $column_num = "n" ]
+				then
+					break 
+				fi
+				# save them in a visited array 
+				column_to_display[$column_num]=1
+			
+			done
+			
+			# now display the selected column needed 
+			fields=$(IFS=,; echo "${!column_to_display[*]}")
+			cut -d: -f"$fields" $2
+			
+			
+			return 
+			
+		    ;;
+		"Exit")
+			return
+		    ;;
+		*)
+		    echo "Invalid option. Try again."
+		    ;;
+	    esac
+	done
+
+
+
+}
+
+conditioned_table(){
+		#$1 = TableName
+		# touch /tmp/tableName copy the table into /tmp/tableName (copy)
+		# 1 - ask for column to condition on 
+		# 2 - show the availiable column number
+		# 3 - enter the column number
+		# 4 - filter the /tmp/tableName
+		# go-to 1 again  done unitl user type n  
+		
+		cp $default_path/$connectedDB/tables/$1  /tmp/$1
+		#cat /tmp/$1
+		echo "*******************"
+		# print columns 
+		while true
+			do
+				
+			columns=$(cut -d ":" -f 1,2 "$default_path/$connectedDB/metadata/$1_meta")
+			num_of_columns=$(wc -l < "$default_path/$connectedDB/metadata/$1_meta")
+			echo "$columns" | nl -s " - " -w 1
+			read -p "enter the column number u want to be conditioned at or (n) to display result : " conditioned_column
+			
+			
+			if [ $conditioned_column -gt  $num_of_columns  ] 
+			then
+				echo "invalid number"
+				continue  
+			fi  
+			
+			
+			
+			if [ $conditioned_column = "n" ] 
+			then
+				break 
+			fi  
+			C_type=$(sed -n "${conditioned_column}p" <<< "$columns" | awk -F: '{print $2}')
+			echo $C_type
+			if [ $C_type = INT ]
+			then
+				select option in "==" ">" "<"; do
+				    case $option in
+					"==")
+					    # awk on /tmp/$1
+					    
+					    read -p "enter value : " value
+					    # awk comparsion
+					    awk -F: -v value="$value" -v column="$conditioned_column" '{ if ($column == value) print }' /tmp/$1  >  /tmp/con_temp
+					    mv /tmp/con_temp /tmp/$1
+					    
+					    read -p "do u want to add another condition (y|n)"  conTinue
+					    
+							      
+					    if [ $conTinue = "y" ] 
+					    then
+					    		break 
+					    
+					    else 
+					    		break 2 
+					    
+					    fi  
+					    
+					    
+					    
+					    ;;
+					">")
+					    
+					
+					    read -p "enter value : " value
+					    # awk comparsion
+					    awk -F: -v value="$value" -v column="$conditioned_column" '{ if ($column > value) print }' /tmp/$1  >  /tmp/con_temp
+					    mv /tmp/con_temp /tmp/$1
+					    
+					    read -p "do u want to add another condition (y|n)"  conTinue
+					    
+							      
+					    if [ $conTinue = "y" ] 
+					    then
+					    		break 
+					    
+					    else 
+					    		break 2 
+					    
+					    fi  
+					    
+					    
+					
+					
+					    break
+					    ;;
+					"<")
+					
+					    read -p "enter value : " value
+					    # awk comparsion
+					    awk -F: -v value="$value" -v column="$conditioned_column" '{ if ($column < value) print }' /tmp/$1  >  /tmp/con_temp
+					    mv /tmp/con_temp /tmp/$1
+					    
+					    read -p "do u want to add another condition (y|n)"  conTinue
+					    
+							      
+					    if [ $conTinue = "y" ] 
+					    then
+					    		break 
+					    
+					    else 
+					    		break 2 
+					    
+					    fi  
+					    
+					    
+					    
+					    break
+					    ;;
+					*)
+					    echo "Invalid option. Try again."
+					    ;;
+				    esac
+				done
+			   else 
+			   # is string 
+			   select option in "==" ; do
+				    case $option in
+					"==")
+					    # awk on /tmp/$1
+					    
+					    read -p "enter value : " value
+					    # awk comparsion
+					    awk -F: -v value="$value" -v column="$conditioned_column" '{ if ($column == value) print }' /tmp/$1  >  /tmp/con_temp
+					    mv /tmp/con_temp /tmp/$1
+					    
+					    read -p "do u want to add another condition (y|n)"  conTinue
+					    
+							      
+					    if [ $conTinue = "y" ] 
+					    then
+					    		break 
+					    
+					    else 
+					    		break 2 
+					    
+					    fi  
+					    
+					    
+					    
+					    ;;
+					"*")
+						"invalid"
+						;;
+			   	      esac
+			   done
+			   
+			   
+			fi
+				
+		 done
+		 
+		 select_display_menu $1 /tmp/$1 $default_path/$connectedDB/metadata/$1_meta
+		 
+		 
+		 
+		
+		
+
+
+
+
+}
+
+select_table(){	
+	while true 
+	do 
+		read -p "Enter Table Name" TableName
+		
+		if [ -f $default_path/$connectedDB/tables/$TableName ]
+		then
+			break 
+		else
+			echo "table doesn't exist "
+		fi
+	done 	
+		select option in "Conditioned" "NotCondition" "Exit"; do
+		    case $option in
+			"Conditioned")
+			    # for conditioned
+			    conditioned_table $TableName
+			    
+			    return 
+			    ;;
+			"NotCondition")
+			     select_display_menu $TableName $default_path/$connectedDB/tables/$TableName  $default_path/$connectedDB/metadata/$TableName_meta
+			     return
+			    ;;
+			"Exit")
+			    
+			    return
+			    ;;
+			*)
+			    echo "Invalid option. Try again."
+			    ;;
+		    esac
+		done
+}
+
+select_table 
+
